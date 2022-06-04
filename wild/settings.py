f5a77c0889ce76c13b6a4d716cccbd3cd7836108
michael-kai <michaelfreeman1818@gmail.com>
2022-06-04 15:14:28 +0300

    Update: Remove BrowsableAPIRenderer

--- wild/settings.py
+++ wild/settings.py
@@ -142,7 +142,7 @@ MEDIA_URL = '/media/'
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer',
+        # 'rest_framework.renderers.BrowsableAPIRenderer',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
